7631586bf4c90b39cd1b405d32b6f3ead6cbd752
jcampos-94 <jcampos.94@hotmail.com>
2026-03-14 11:39:00 -0500

    .gitignore updated

--- src/models/Faction.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Data model representing a group or organization within the world.
- */
-export declare class Faction {
-    name: string;
-    leaderName: string;
-    constructor(name: string, leaderName: string);
-}
-//# sourceMappingURL=Faction.d.ts.map
